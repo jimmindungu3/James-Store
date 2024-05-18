93db1ab8e2acd5a76d553f9cd2494836ad784bf7
jimmindungu3 <jimmindungu3@gmail.com>
2024-05-18 16:02:33 +0300

    implement modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { PiShoppingCartSimpleBold } from "react-icons/pi";
+
+const Modal = ({ selectedProduct, closeModal }) => {
+  return (
+    <div>
+      <div className="fixed inset-0 z-2 flex justify-center items-center bg-gray-800 bg-opacity-50">
+        <div className="p-4 bg-white rounded-lg flex flex-col justify-between items-center max-w-[450px] relative">
+          <div className="flex justify-between w-full">
+            <div className="font-bold">{selectedProduct.title}</div>
+            <button></button>
+          </div>
+          <IoCloseCircleOutline
+            onClick={() => closeModal()}
+            className="text-red-900 text-4xl font-bold absolute top-2 right-2 cursor-pointer"
+          />
+          <div className="mb-4 h-60 w-full flex justify-center items-center">
+            <img
+              className="h-full mt-6"
+              src={selectedProduct.image}
+              alt={selectedProduct.title}
+            />
+          </div>
+          <div className="mt-4">{selectedProduct.description}</div>
+          <div className="mt-4 flex justify-between items-center w-full">
+            <div className="font-bold text-blue-600">
+              $ {selectedProduct.price}
+            </div>
+            <div className="inline-flex rounded-md shadow-sm">
+              <button
+                type="button"
+                className="px-4 py-2 text-lg font-bold bg-gray-300 hover:bg-gray-400 active:bg-gray-300 rounded-l-md"
+              >
+                +
+              </button>
+              <button
+                disabled
+                type="button"
+                className="px-4 py-2 text-lg font-bold bg-gray-300"
+              >
+                <PiShoppingCartSimpleBold />
+              </button>
+              <button
+                type="button"
+                className="px-4 py-2 text-lg font-bold bg-gray-300 hover:bg-gray-400 active:bg-gray-300 rounded-r-md"
+              >
+                -
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,6 @@
 import { PiShoppingCartSimpleBold } from "react-icons/pi";
-import { IoCloseCircleOutline } from "react-icons/io5"
+
+import Modal from "./Modal";
 
 import React, { useState } from "react";
 import Loader from "./Loader";
@@ -90,7 +91,7 @@ const Products = ({ products, APISetter, loading }) => {
                     <div className="mt-4">
                       {truncateWords(product.description, 20)}{" "}
                       <span
-                        className="text-blue-600 text-nowrap cursor-pointer"
+                        className="text-blue-600 text-nowrap hover:cursor-pointer"
                         onClick={() => openModal(product)}
                       >
                         More details
@@ -130,52 +131,10 @@ const Products = ({ products, APISetter, loading }) => {
         </div>
       </div>
 
-      {/* Modal starts here */}
+      {/* Modal */}
       {selectedProduct && (
-  <div className="fixed inset-0 z-2 flex justify-center items-center bg-gray-800 bg-opacity-50">
-    <div className="p-4 bg-white rounded-lg flex flex-col justify-between items-center max-w-[450px]">
-      <div className="flex justify-between w-full">
-        <div className="font-bold">{selectedProduct.title}</div>
-        <IoCloseCircleOutline onClick={() => closeModal()} className="cursor-pointer text-red-900 text-4xl font-bold" />
-      </div>
-      <div className="mb-4 h-60 w-full flex justify-center items-center">
-        <img
-          className="h-full mt-6"
-          src={selectedProduct.image}
-          alt={selectedProduct.title}
-        />
-      </div>
-      <div className="mt-4">{selectedProduct.description}</div>
-      <div className="mt-4 flex justify-between items-center w-full">
-        <div className="font-bold text-blue-600">
-          $ {selectedProduct.price}
-        </div>
-        <div className="inline-flex rounded-md shadow-sm">
-          <button
-            type="button"
-            className="px-4 py-2 text-lg font-bold bg-gray-300 hover:bg-gray-400 active:bg-gray-300 rounded-l-md"
-          >
-            +
-          </button>
-          <button
-            disabled
-            type="button"
-            className="px-4 py-2 text-lg font-bold bg-gray-300"
-          >
-            <PiShoppingCartSimpleBold />
-          </button>
-          <button
-            type="button"
-            className="px-4 py-2 text-lg font-bold bg-gray-300 hover:bg-gray-400 active:bg-gray-300 rounded-r-md"
-          >
-            -
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
-)}
-
+        <Modal selectedProduct={selectedProduct} closeModal={closeModal} />
+      )}
     </div>
   );
 };
