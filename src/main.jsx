9cbeaf3ba965961ca58e464c331e85e7af9b09db
jimmindungu3 <jimmindungu3@gmail.com>
2024-05-21 18:29:51 +0300

    render card

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
+// import App from "./App.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import Home from "./pages/Home.jsx";
 import Cart from "./components/Cart.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   { path: "/shop", element: <Home /> },
   { path: "/cart", element: <Cart /> },
+  { errorElement: <NotFound /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
