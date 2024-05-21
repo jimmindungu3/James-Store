c3f1c7956ab1d254c7ef1bd87b154cfc341cfb12
jimmindungu3 <jimmindungu3@gmail.com>
2024-05-21 18:10:22 +0300

    add clientside routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Home from "./pages/Home.jsx";
+import Cart from "./components/Cart.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/shop", element: <Home /> },
+  { path: "/cart", element: <Cart /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
