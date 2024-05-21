6b82c7d693fdec8e663139a1a82d232c1f70646d
jimmindungu3 <jimmindungu3@gmail.com>
2024-05-21 18:28:24 +0300

    create not found page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-300">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">
+          404 Error! 😢 The page you're looking for does not exist.
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
